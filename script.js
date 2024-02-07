a7cc0d8f766aa4ca1e3a1b9fc9720ae37654bafe
Ravi-Kumar-Yedla <136640114+Ravi-Kumar-Yedla@users.noreply.github.com>
2024-02-07 16:12:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 // let currentUrl = window.location.href;
 //  let length  = currentUrl.length;
 // alert(`The length of the URL is ${length}`)
+// Get the current URL
+		let currentUrl = window.location.href;
+
+		// Get the length of the URL
+		let urlLength = currentUrl.length;
+
+		// Log the length of the URL to the console
+		alert("The length of the URL is: " + urlLength);
